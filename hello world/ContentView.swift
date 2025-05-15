8ae0c33685cb027e87bb0314bb81d9ca1adbed95
Gaya-Avetisyan <gaya.avetisian@gmail.com>
2025-05-15 22:39:33 +0400

    Initial Commit

---
+++ hello world/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello world
+//
+//  Created by Gayane Avetisyan on 15.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello world
+//
+//  Created by Gayane Avetisyan on 15.05.25.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
